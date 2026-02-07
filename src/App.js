bec06ec6df286dab85853d3e2ba6068ea13b7175
kamesh <kamesh@gmail.com>
2026-02-08 01:33:09 +0530

    uploaded style file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Gallery from "./components/Gallery"
 import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+
 function App() {
   return (
     <div>
-      <Navbar/>
+      <h1 className="title">Dynamic Image Gallery</h1>
       <Gallery />
       <Footer />
     </div>
--- src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-function Navbar() {
-  return (
-    <nav>
-    <h1 className="title">Dynamic Image Gallery</h1>
-    </nav>
-  )
-}
-
-export default Navbar
